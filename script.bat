befef9b82284fb6b2c63c22049628b3d0fbb4169
Saurav Dudhate <75058098+saurav2127@users.noreply.github.com>
2023-07-18 17:34:55 +0530

    Delete script.bat

--- script.bat
+++
@@ -1,2 +0,0 @@
-@For /F "UseBackQ Delims=" %%A In ("./extracted-urls.txt"
-) Do @LightHouse "%%A"  --disable-storage-reset --emulated-form-factor=desktop   --throttling-method=provided  --view --only-categories=performance,accessibility --output=json --output=html
